b4f90a1781cc1359a69ca066fb0cc14e03253b6d
Lynn Buchanan <lynnbuchananesq@gmail.com>
2025-12-26 19:13:28 -0500

    finished cli

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,82 +1,51 @@
 package config
 
-import {
+import (
 	"fmt"
 	"os"
 	"encoding/json"
-	"errors"
-}
+	"path/filepath"
+)
 
 type Config struct {
-	db_url string `json:"db_url"`
-	current_user_name string `json:"current_user_name"`
+	DBURL string `json:"db_url"`
+	CurrentUsername string `json:"current_user_name"`
 }
 
-type State struct {
-	config *Config
-}
-
-type Command struct {
-	name string
-	arguments []string
-}
-
-type Commands struct {
-	list map[string]func(*state, command) error
-}
-
-func (c *commands) Run(s *state, cmd command) error {
-	err := c.name.cmd(s, cmd)
-	if err {
-		return fmt.Errorf("error running cmd: %v: %v", c.name, err)
+func Read() (Config, error) {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return Config{}, fmt.Errorf("error getting user home directory: %v", err)
 	}
+	path := filepath.Join(homeDir, ".gatorconfig.json")
+	c := Config{}
+	file, err := os.Open(path)
+	if err != nil {
+		return Config{}, fmt.Errorf("error opening config file at %v: %v", homeDir, err)
+	}
+	defer file.Close()
+	d := json.NewDecoder(file)
+	err = d.Decode(&c)
+	if err != nil {
+		return Config{}, fmt.Errorf("error unmarshaling json: %v", err)
+	}
+	return c, nil
 }
 
-func (c *commands Register(name string, f func(*state, command) error {
-	c.list[name] = f
-	return nil
-}
-
-func Read() *Config, error {
-	homeDir := os.UserHomeDir()
-	c := &Config{}
-	file, err := os.Open(homeDir) {
-		if err {
-			return fmt.Errorf("error opening config file at %v: %v", hokeDir, err)
-		}
+func (c *Config) SetUser(username string) error {
+	c.CurrentUsername = username
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return fmt.Errorf("error getting user home directory: %v", err)
 	}
-	cont, err := file.Read(homeDir + "/.gatorconfig.json") {
-		if err {
-			return fmt.Errorf("error reading confif file at %v: %v", homeDir, err)
-		}
+	path := filepath.Join(homeDir, ".gatorconfig.json")
+	file, err2 := json.Marshal(c)
+	if err2 != nil {
+		return fmt.Errorf("error marshaling config struct: %v", err2)
 	}
-	if err := json.Unmarshal(cont, &c); err == true {
-		return fmt.Errorf("error unmarshaling json: %v", err)
+	err3 := os.WriteFile(path, file, 0644)
+	if err3 != nil {
+		return fmt.Errorf("error writing file: %v", err3)
 	}
-	return c
-}
-
-func (c Config) SetUser(username string) error {
-	c.current_user_name = username
-	homeDir := os.UserHomeDir()
-	file, err := json.Marshal(c)
-	if err {
-		return fmt.Errorf("error marshaling config struct: %v", err)
-	}
-	err2 := os.WriteFile("gatorconfig.json", file)
-	if err2 {
-		return fmt.Errorf("error writing file: %v", err)
-	}
-}
-
-func HandlerLogin(s *state, cmd command) error {
-	if len(cmd.arguments) < 1 {
-		return errors.New("error logging in: current username not found")
-}
-	err := s.config.SetUser(cmd.arguments[0])
-	if err {
-		return fmt.Errorf("error setting username: %v", err)
-	}
-	fmt.Printf("current user: %v has been set", cmd.argument[0])
 	return nil
 }
--- main.go
+++ main.go
@@ -1,19 +1,82 @@
 package main
 
-import {
-	cfg "bloggatog/internal/config/.gatorconfig"
+import (
+	cfg "github.com/bloggatog/internal/config"
 	"fmt"
 	"os"
+	"errors"
+)
+
+type state struct {
+	config *cfg.Config
+}
+
+type command struct {
+	Name string
+	Arguments []string
+}
+
+type commands struct {
+	list map[string]func(*state, command) error
+}
+
+func (c *commands) Run(s *state, cmd command) error {
+	handler, ok := c.list[cmd.Name]
+	if !ok {
+		return fmt.Errorf("error running cmd: %v", cmd.Name)
+	}
+	err := handler(s, cmd)
+	if err != nil {
+		return fmt.Errorf("error executing command %v: %v", cmd.Name, err)
+	}
+	return nil
+}
+
+func (c *commands) Register(name string, f func(*state, command) error) {
+	c.list[name] = f
+}
+
+func HandlerLogin(s *state, cmd command) error {
+	if len(cmd.Arguments) < 1 {
+		return errors.New("error logging in: current username not found")
+}
+	err := s.config.SetUser(cmd.Arguments[0])
+	if err != nil {
+		return fmt.Errorf("error setting username: %v", err)
+	}
+	fmt.Printf("current user: %v has been set\n", cmd.Arguments[0])
+	return nil
 }
 
 func main() {
-	// create a buffered io reader
-	cont, err := cfg.Read()
-	if err {
+	config, err := cfg.Read()
+	if err != nil {
 		fmt.Printf("error reading config: %v", err)
+		os.Exit(1)
+
 	}
-	s := &cfg.State{config: cont}
-	commands := cfg.Commands{}
-	commands.Register("login", login(s, os.Args[1]))
-	continue
+
+	s := &state{config: &config}
+
+	commands := commands{
+		list: make(map[string]func(*state, command) error),
+	}
+
+	commands.Register("login", HandlerLogin)
+
+	args := os.Args
+	if len(args) < 2 {
+		fmt.Println("not enough arguments provided")
+		os.Exit(1)
+	}
+
+	cmd := command{
+		Name:      args[1],
+		Arguments: args[2:],
+	}
+
+	if err := commands.Run(s, cmd); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
